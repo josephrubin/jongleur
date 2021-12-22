703940fdcfa602b496553ef4534652e62bc91cce
Joseph Rubin <josephmeirrubin@gmail.com>
2021-12-21 19:47:55 -0500

    b

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ import matplotlib.pyplot as plt
 
 def _main():
   filename = sys.argv[1]
+  if not filename.endswith('ogg'):
+    print(
+      "Please convert your audio file to .ogg (ogg vorbis) first "
+      "(possibly by using ffmpeg)."
+    )
   print(filename)
 
-  demofilename = librosa.example('nutcracker')
+  #demofilename = librosa.example('nutcracker')
 
+  # Load the file, using a lower sampling rate so it will go faster.
   waveform, samplingrate = librosa.load(filename)
   #print(waveform, samplingrate)
 
@@ -21,9 +27,12 @@ def _main():
   beat_times = librosa.frames_to_time(beat_frames, sr=samplingrate)
   #print(beat_times)
 
-  plt.figure()
-  librosa.display.waveplot(y=waveform, sr=samplingrate)
-  plt.show()
+  #plt.figure()
+  #librosa.display.waveplot(y=waveform, sr=samplingrate)
+  #plt.show()
+
+  segments = librosa.effects.split(waveform)
+  print("segs", segments)
 
 if __name__ == '__main__':
   _main()
